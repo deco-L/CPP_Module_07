91edb0220f667b4d69de11b739d8a446032cbd22
deco-L <scotchwitchbroom@gmail.com>
2024-06-28 12:38:11 +0900

    finish ex00

--- ex00/include/whatever.hpp
+++ ex00/include/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/05/27 14:58:48 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/06/28 12:37:37 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,27 @@
 
 #include <cstdlib>
 
-class Whatever
+template <typename T>
+void swap( T& lhs, T& rhs )
 {
-};
+  T tmp;
+
+  tmp = lhs;
+  lhs = rhs;
+  rhs = tmp;
+  return ;
+}
+
+template <typename T>
+const T& min( const T& lhs, const T& rhs )
+{
+  return ((lhs < rhs) ? lhs : rhs);
+}
+
+template <typename T>
+const T& max( const T& lhs, const T& rhs )
+{
+  return ((lhs > rhs) ? lhs : rhs);
+}
 
 #endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,12 +6,45 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/05/27 15:04:18 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/06/28 12:37:53 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+#include <iostream>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+static size_t get_term_line() {
+	winsize	terminal_size;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &terminal_size);
+	return (terminal_size.ws_col);
+}
+
+static void draw_terminal_line() {
+	size_t	terminal_col;
+
+	terminal_col = get_term_line();
+	for (size_t i = 0; i < terminal_col; i++)
+		std::cout << "=";
+	std::cout << std::endl;
+	return ;
+}
 
 int main(void) {
+  draw_terminal_line();
+  int a = 2;
+  int b = 3;
+  ::swap( a, b );
+  std::cout << "a = " << a << ", b = " << b << std::endl;
+  std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+  std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+  std::string c = "chaine1";
+  std::string d = "chaine2";
+  ::swap(c, d);
+  std::cout << "c = " << c << ", d = " << d << std::endl;
+  std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+  std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+  draw_terminal_line();
   return (EXIT_SUCCESS);
 }
