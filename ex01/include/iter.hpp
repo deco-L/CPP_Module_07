ae81ef3ca58595f083999256b42ba610913170d3
deco-L <scotchwitchbroom@gmail.com>
2024-06-28 14:07:00 +0900

    finish ex01

--- ex01/include/iter.hpp
+++ ex01/include/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/05/27 16:04:45 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/06/28 13:50:05 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 #define ITER_HPP
 
 #include <cstdlib>
+
+template <typename T, typename Func>
+void  iter(T* array, const std::size_t length, Func func)
+{
+  for (std::size_t index = 0; index < length; index++)
+    func(array[index]);
+  return ;
+}
 
 #endif
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,12 +6,45 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/05/27 16:03:48 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/06/28 13:52:05 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
+#include <iostream>
+#include <sstream>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
-int main(void) {
+static size_t get_term_line() {
+	winsize	terminal_size;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &terminal_size);
+	return (terminal_size.ws_col);
+}
+
+static void draw_terminal_line() {
+	size_t	terminal_col;
+
+	terminal_col = get_term_line();
+	for (size_t i = 0; i < terminal_col; i++)
+		std::cout << "=";
+	std::cout << std::endl;
+	return ;
+}
+
+void intToString(const std::string& str)
+{
+  std::cout << str << " ";
+}
+
+int main() {
+  draw_terminal_line();
+  std::string array[] = {"apple", "banana", "cherry"};
+  std::size_t length = sizeof(array) / sizeof(array[0]);
+
+  std::cout << "String array: ";
+  iter(array, length, intToString);
+  std::cout << std::endl;
+  draw_terminal_line();
   return (EXIT_SUCCESS);
 }
